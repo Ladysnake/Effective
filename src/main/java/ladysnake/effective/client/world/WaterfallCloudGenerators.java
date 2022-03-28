2c0a036e412236509a02d92d0afb0c488c886628
doctor4t <25477005+doctor4t@users.noreply.github.com>
2022-03-28 21:48:11 +0200

    Add an adaptive audio calculation for waterfall sounds

--- src/main/java/ladysnake/effective/client/world/WaterfallCloudGenerators.java
+++ src/main/java/ladysnake/effective/client/world/WaterfallCloudGenerators.java
@@ -3,8 +3,10 @@ package ladysnake.effective.client.world;
 import ladysnake.effective.client.Effective;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
@@ -37,11 +39,18 @@ public class WaterfallCloudGenerators {
                 generatorsToRemove.add(waterfallCloudGenerator);
             }
 
-            if (world.random.nextInt(1) == 0 && world.getBlockState(blockPos).getBlock() == Blocks.WATER && world.getBlockState(blockPos).getFluidState().isStill() && world.getBlockState(blockPos.add(0, 1, 0)).getBlock() == Blocks.WATER && !world.getBlockState(blockPos.add(0, 1, 0)).getFluidState().isStill() && world.getBlockState(blockPos.add(0, 1, 0)).getFluidState().getHeight() >= 0.77f) {
+            if (world.getBlockState(blockPos).getBlock() == Blocks.WATER && world.getBlockState(blockPos).getFluidState().isStill() && world.getBlockState(blockPos.add(0, 1, 0)).getBlock() == Blocks.WATER && !world.getBlockState(blockPos.add(0, 1, 0)).getFluidState().isStill() && world.getBlockState(blockPos.add(0, 1, 0)).getFluidState().getHeight() >= 0.77f) {
                 double offsetX = world.random.nextGaussian() / 5f;
                 double offsetZ = world.random.nextGaussian() / 5f;
 
-                world.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), Effective.AMBIENCE_WATERFALL, SoundCategory.AMBIENT, 2.5f, 1.2f + world.random.nextFloat() / 10f, true);
+                if (world.random.nextInt(5) == 0) {
+                    Entity camera = MinecraftClient.getInstance().getCameraEntity();
+                    double distance = Math.sqrt(camera.getBlockPos().getSquaredDistance(blockPos));
+                    if (distance <= 80) {
+                        world.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(), Effective.AMBIENCE_WATERFALL, SoundCategory.AMBIENT, (float) MathHelper.lerp(distance / 80F, .05, 6), 1.2f + world.random.nextFloat() / 10f, true);
+                    }
+                }
+
                 world.addParticle(Effective.WATERFALL_CLOUD, blockPos.getX() + .5 + offsetX, blockPos.getY() + 1 + world.random.nextFloat(), blockPos.getZ() + .5 + offsetZ, world.random.nextFloat() / 5f * Math.signum(offsetX), world.random.nextFloat() / 5f, world.random.nextFloat() / 5f * Math.signum(offsetZ));
             }
         }
