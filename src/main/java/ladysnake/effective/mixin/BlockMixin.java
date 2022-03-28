d55fb4a2ddd55ed70869e686cf13d007eed397e5
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-03-28 16:29:59 -0500

    remove unused shadow

--- src/main/java/ladysnake/effective/mixin/BlockMixin.java
+++ src/main/java/ladysnake/effective/mixin/BlockMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,10 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Random;
 
 @Mixin(Block.class)
-public abstract class BlockMixin {
-    @Shadow
-    public abstract BlockState getDefaultState();
-
+public class BlockMixin {
     @Inject(method = "randomDisplayTick", at = @At("RETURN"))
     protected void effective$randomDisplayTick(BlockState state, World world, BlockPos pos, Random random, CallbackInfo ci) {
         if (state.getBlock() == Blocks.WATER && !world.getBlockState(pos.add(0, 1, 0)).getFluidState().isStill() && world.getBlockState(pos.add(0, 1, 0)).getBlock() == Blocks.WATER && !world.getBlockState(pos.add(0, 1, 0)).getFluidState().isStill() && world.getBlockState(pos.add(0, 1, 0)).getFluidState().getHeight() >= 0.77f) {
