c5faec18601237addaa8fa4f1bf413718606bdb4
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-03-30 15:29:16 -0500

    move to FluidRenderer and stop checking twice

--- src/main/java/ladysnake/effective/mixin/FluidRendererMixin.java
+++ src/main/java/ladysnake/effective/mixin/FluidRendererMixin.java
@@ -4,7 +4,7 @@ import ladysnake.effective.client.world.WaterfallCloudGenerators;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.block.BlockRenderManager;
+import net.minecraft.client.render.block.FluidRenderer;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
@@ -13,12 +13,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BlockRenderManager.class)
-public class BlockRenderManagerMixin {
-    @Inject(method = "renderFluid", at = @At("TAIL"))
-    public void renderFluid(BlockPos pos, BlockRenderView world, VertexConsumer vertexConsumer, BlockState blockState, FluidState fluidState, CallbackInfoReturnable<Boolean> cir) {
-        if (WaterfallCloudGenerators.shouldCauseWaterfall(world, pos)) {
-            WaterfallCloudGenerators.addGenerator(MinecraftClient.getInstance().world, pos);
-        }
+@Mixin(FluidRenderer.class)
+public class FluidRendererMixin {
+    @Inject(method = "render", at = @At("TAIL"))
+    public void renderFluid(BlockRenderView world, BlockPos pos, VertexConsumer vertexConsumer, BlockState blockState, FluidState fluidState, CallbackInfoReturnable<Boolean> cir) {
+        WaterfallCloudGenerators.addGenerator(MinecraftClient.getInstance().world, pos);
     }
 }
