5b4aa7ee8e3052454a4349bf791171be5d6ac67b
Hedreon <39467046+Hedreon@users.noreply.github.com>
2022-08-07 14:39:13 +0100

    Add option

--- src/main/java/ladysnake/effective/client/ModConfig.java
+++ src/main/java/ladysnake/effective/client/ModConfig.java
@@ -9,6 +9,7 @@ public class ModConfig implements ConfigData {
 	public boolean generateCascades = true;
 	public boolean generateSplashes = true;
 	public boolean shouldFlowingWaterSpawnParticlesOnFirstTick = true;
+	public boolean muteWaterfalls = false;
 	@ConfigEntry.BoundedDiscrete(max = 100)
 	public long lapisBlockUpdateParticleChance = 10;
 }
