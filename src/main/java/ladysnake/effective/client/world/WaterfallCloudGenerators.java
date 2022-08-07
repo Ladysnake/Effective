914d8b9460ca5583c0b9a99fc254d677af7f0ae6
Hedreon <39467046+Hedreon@users.noreply.github.com>
2022-08-07 15:14:34 +0100

    Update WaterfallCloudGenerators.java

--- src/main/java/ladysnake/effective/client/world/WaterfallCloudGenerators.java
+++ src/main/java/ladysnake/effective/client/world/WaterfallCloudGenerators.java
@@ -49,9 +49,12 @@ public class WaterfallCloudGenerators {
                 BlockPos pos = generators.get(i);
                 if (pos != null) {
                     if (shouldCauseWaterfall(world, pos, world.getFluidState(pos))) {
-                        if (world.random.nextInt(200) == 0) {
-                            world.playSound(pos.getX(), pos.getY(), pos.getZ(), Effective.AMBIENCE_WATERFALL, SoundCategory.AMBIENT, 10f, 1.2f + world.random.nextFloat() / 10f, true);
+                        if (Effective.config.MuteWaterfalls) {
+                            if (world.random.nextInt(200) == 0) {
+                                world.playSound(pos.getX(), pos.getY(), pos.getZ(), Effective.AMBIENCE_WATERFALL, SoundCategory.AMBIENT, 10f, 1.2f + world.random.nextFloat() / 10f, true);
+                            }
                         }
+                        
                         scheduleParticleTick(pos, 6);
                     }
                     else {
