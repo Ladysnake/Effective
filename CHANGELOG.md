4c65625ba8209d07ddfdeed996cdf6367e0d9dac
doctor4t <25477005+doctor4t@users.noreply.github.com>
2022-08-28 22:08:19 +0200

    Add a new config option to adjust cascade sounds volume

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Effective - Changelog:
 
-## Effective 1.2.3 - 1.19.2
+## Effective 1.3 - 1.19.2
+- Added a new config option to adjust cascade sounds volume
 - Fixed splashes sometimes being completely black (thanks to lonefelidae16)
 - Fixed occasional crashes that could happen randomly with cascades (thanks to lonefelidae16)
 - Changed config library from Cloth Config to MidnightLib
--- src/main/java/ladysnake/effective/client/EffectiveConfig.java
+++ src/main/java/ladysnake/effective/client/EffectiveConfig.java
@@ -4,14 +4,18 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class EffectiveConfig extends MidnightConfig {
     @Entry
-    public static boolean generateCascades = true;
+    public static boolean generateSplashes = true;
 
     @Entry
-    public static boolean generateSplashes = true;
+    public static boolean generateCascades = true;
+
+    @Entry(min = 0, max = 500)
+    public static int cascadeSoundsVolumeMultiplier = 100;
 
     @Entry
     public static boolean shouldFlowingWaterSpawnParticlesOnFirstTick = true;
 
     @Entry(min = 0, max = 100)
     public static int lapisBlockUpdateParticleChance = 10;
+
 }
