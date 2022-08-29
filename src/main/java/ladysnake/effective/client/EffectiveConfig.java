b20eded68833c03613ca601339ca141ec7ab6128
doctor4t <25477005+doctor4t@users.noreply.github.com>
2022-08-29 10:54:30 +0200

    Increase default waterfall sound distance config value + config max waterfall sound distance

--- src/main/java/ladysnake/effective/client/EffectiveConfig.java
+++ src/main/java/ladysnake/effective/client/EffectiveConfig.java
@@ -12,9 +12,9 @@ public class EffectiveConfig extends MidnightConfig {
     @Entry(min = 0, max = 500)
     public static int cascadeSoundsVolumeMultiplier = 100;
 
-    @Entry(min = 0, max = 150)
-    public static int waterfallSoundDistanceBlocks = 36;
-
+    @Entry(min = 0, max = 400)
+    public static int waterfallSoundDistanceBlocks = 150;
+    
     @Entry
     public static boolean shouldFlowingWaterSpawnParticlesOnFirstTick = true;
 
