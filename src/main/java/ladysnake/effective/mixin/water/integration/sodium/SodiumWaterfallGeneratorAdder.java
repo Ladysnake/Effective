3cf5eef725488711b9f7d09404bbf43112729b8c
doctor4t <25477005+doctor4t@users.noreply.github.com>
2023-06-14 19:10:23 +0200

    Fix Sodium compat mixin

--- src/main/java/ladysnake/effective/mixin/water/integration/sodium/SodiumWaterfallGeneratorAdder.java
+++ src/main/java/ladysnake/effective/mixin/water/integration/sodium/SodiumWaterfallGeneratorAdder.java
@@ -1,21 +1,20 @@
 package ladysnake.effective.mixin.water.integration.sodium;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import ladysnake.effective.world.WaterfallCloudGenerators;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.render.block.FluidRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.buffers.ChunkModelBuilder;
+import me.jellysquid.mods.sodium.client.render.pipeline.FluidRenderer;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FluidRenderer.class)
 public class SodiumWaterfallGeneratorAdder {
 	@Inject(method = "render", at = @At("HEAD"))
-	public void effective$generateWaterfall(BlockRenderView world, BlockPos pos, VertexConsumer vertexConsumer, BlockState blockState, FluidState state, CallbackInfo ci) {
-		WaterfallCloudGenerators.addGenerator(state, pos.toImmutable());
+	public void effective$generateWaterfall(BlockRenderView world, FluidState fluidState, BlockPos pos, BlockPos offset, ChunkModelBuilder buffers, CallbackInfoReturnable<Boolean> cir) {
+		WaterfallCloudGenerators.addGenerator(fluidState, pos.toImmutable());
 	}
 }
