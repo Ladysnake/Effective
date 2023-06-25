e3992dd72b129e84f19c890ef5777a3726bc63f4
doctor4t <25477005+doctor4t@users.noreply.github.com>
2023-06-25 16:14:20 +0200

    Fix lingering potion effects using the improved dragon breath particles

--- src/main/java/ladysnake/effective/mixin/fireballs/DragonBreathRendererSwapper.java
+++ src/main/java/ladysnake/effective/mixin/fireballs/DragonBreathRendererSwapper.java
@@ -10,6 +10,7 @@ import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,7 +25,7 @@ public abstract class DragonBreathRendererSwapper extends Entity {
 
 	@WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addImportantParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
 	private void effective$swapDragonBreathParticles(World world, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, Operation<Void> voidOperation) {
-		if (EffectiveConfig.improvedDragonFireballsAndBreath) {
+		if (EffectiveConfig.improvedDragonFireballsAndBreath && parameters == ParticleTypes.DRAGON_BREATH) {
 			ParticleBuilders.create(Effective.DRAGON_BREATH)
 				.setSpin((float) (world.random.nextGaussian() / 5f))
 				.setScale(1f, 0f)
