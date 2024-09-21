fcf8392f1876d9421fd69df0d128c889f48bdda1
doctor4t <doctor4t@gmail.com>
2024-09-21 19:54:25 +0200

    Add cave, lush cave, dripstone cave and cricket sounds

--- src/client/java/org/ladysnake/effective/ambience/EffectiveAmbience.java
+++ src/client/java/org/ladysnake/effective/ambience/EffectiveAmbience.java
@@ -2,6 +2,8 @@ package org.ladysnake.effective.ambience;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeKeys;
 import org.ladysnake.effective.ambience.sound.AmbientCondition;
 import org.ladysnake.effective.core.Effective;
@@ -14,9 +16,19 @@ public class EffectiveAmbience implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_PLAINS_DAY, (world, pos, player) -> world.getBiome(pos).isIn(ConventionalBiomeTags.PLAINS) && !Effective.isNightTime(world))); // plains day
-		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_BEACH, (world, pos, player) -> world.getBiome(pos).isIn(ConventionalBiomeTags.BEACH))); // beach
+		/* SURFACE */
+		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_PLAINS_DAY, (world, pos, player) -> !isInCave(world, pos) && world.getBiome(pos).isIn(ConventionalBiomeTags.CLIMATE_TEMPERATE) && !Effective.isNightTime(world))); // plains day
+		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_BEACH, (world, pos, player) -> !isInCave(world, pos) && world.getBiome(pos).isIn(ConventionalBiomeTags.BEACH))); // beach
+		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.CRICKETS, (world, pos, player) -> !isInCave(world, pos) && world.getBiome(pos).isIn(ConventionalBiomeTags.CLIMATE_TEMPERATE) && Effective.isNightTime(world))); // crickets for night time in temperate biomes
+
+		/* CAVES */
+		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_CAVE, (world, pos, player) -> isInCave(world, pos) && !world.isSkyVisible(pos))); // cave
 		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_DEEP_DARK, (world, pos, player) -> world.getBiome(pos).matchesKey(BiomeKeys.DEEP_DARK))); // deep dark
-		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.CRICKETS, (world, pos, player) -> world.getBiome(pos).isIn(ConventionalBiomeTags.CLIMATE_TEMPERATE) && Effective.isNightTime(world))); // crickets for night time in temperate biomes
+		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_LUSH_CAVE, (world, pos, player) -> world.getBiome(pos).matchesKey(BiomeKeys.LUSH_CAVES))); // lush caves
+		AMBIENT_CONDITIONS.add(new AmbientCondition(EffectiveAmbienceSounds.AMBIENT_DRIPSTONE_CAVE, (world, pos, player) -> world.getBiome(pos).matchesKey(BiomeKeys.DRIPSTONE_CAVES))); // dripstone caves
+	}
+
+	public static final boolean isInCave(World world, BlockPos pos) {
+		return pos.getY() <= world.getSeaLevel();
 	}
 }
--- src/client/java/org/ladysnake/effective/ambience/EffectiveAmbienceSounds.java
+++ src/client/java/org/ladysnake/effective/ambience/EffectiveAmbienceSounds.java
@@ -11,10 +11,16 @@ import java.util.List;
 public class EffectiveAmbienceSounds {
 	protected static final List<SoundEvent> SOUND_EVENTS = new LinkedList<>();
 
+	/* SURFACE */
 	public static final SoundEvent AMBIENT_PLAINS_DAY = create("ambient.plains.day");
 	public static final SoundEvent AMBIENT_BEACH = create("ambient.beach");
-	public static final SoundEvent AMBIENT_DEEP_DARK = create("ambient.deep_dark");
 	public static final SoundEvent CRICKETS = create("ambient.crickets");
+
+	/* CAVES */
+	public static final SoundEvent AMBIENT_CAVE = create("ambient.cave");
+	public static final SoundEvent AMBIENT_DEEP_DARK = create("ambient.deep_dark");
+	public static final SoundEvent AMBIENT_LUSH_CAVE = create("ambient.lush_cave");
+	public static final SoundEvent AMBIENT_DRIPSTONE_CAVE = create("ambient.dripstone_cave");
 
 	protected static SoundEvent create(String name) {
 		SoundEvent soundEvent = SoundEvent.of(Effective.id(name));
